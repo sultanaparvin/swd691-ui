f08e0bf4bcc8a80dcf668ae8d1bd8661b4d58d5d
sultanaparvin <33079103+sultanaparvin@users.noreply.github.com>
2020-02-05 08:57:24 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 
     </main>
     <footer>
-        <span class="copyright">Copyright 2019 by Sultana Parvin</span>
+        <span class="copyright">Copyright 2020 by Sultana Parvin</span>
     </footer>
 </body>
 </html>
